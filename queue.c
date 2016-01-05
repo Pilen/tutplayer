08b9075d11574bc1e3cf2158b1c1d7806eb145a2
Pilen <fiskomaten@gmail.com>
2016-01-05 03:25:28 +0100

    Introduce a writelock for the queue instead of using a compare_and_swap/compare_exchange. Only writers use this lock, readers are independent, thus still requiring atomic operations

--- queue.c
+++ queue.c
@@ -16,6 +16,7 @@ struct Queue_s {
     Queue_node *all; /* Only used for cleaning */
     Queue_node *read;
     Queue_node *last;
+    pthread_mutex_t write_lock;
     unsigned int length;
 };
 
@@ -26,32 +27,24 @@ Queue *queue_create(void) {
     queue -> read = node;
     queue -> last = node;
 
+    pthread_mutex_init(&queue -> write_lock, NULL);
+
     return queue;
 }
 
 void queue_add(Queue *queue, void *data) {
+    /* In the critical section, the queue is in a slightly inconsistent state
+       where the length might differ*/
     t_assert(queue);
     Queue_node *node = TUT_MALLOCZ(Queue_node);
     node -> data = data;
     node -> next = NULL;
 
-    /* Add node to queue atomically */
-    Queue_node *last = queue -> last;
-    Queue_node *expected = NULL;
-    do {
-        expected = NULL;
-        while (last -> next != NULL) {
-            last = last -> next;
-        }
-    } while (__atomic_compare_exchange(&last -> next, &expected, &node, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST) == false);
-
-    /* From now on the queue is in a slightly inconsistent state
-       the length might differ and the last pointer might not point to the actual last.
-       But by following it manually and using the length as approximate it is okay*/
-
-    __atomic_store(&queue -> last, &node, __ATOMIC_SEQ_CST);
-    /* Update length */
+    pthread_mutex_lock(&queue -> write_lock);
+    queue -> last = node;
+    __atomic_store(&queue -> last -> next, &node, __ATOMIC_SEQ_CST);
     __atomic_add_fetch(&queue -> length, 1, __ATOMIC_SEQ_CST);
+    pthread_mutex_unlock(&queue -> write_lock);
 }
 
 void *queue_read(Queue *queue) {
@@ -71,17 +64,20 @@ void *queue_read(Queue *queue) {
 }
 
 unsigned int queue_cleanup(Queue *queue) {
-    t_assert(false);
+    t_assert(queue);
+    unsigned int cleaned = 0;
+    pthread_mutex_lock(&queue -> write_lock);
     Queue_node *node = queue -> all;
     Queue_node *read;
     __atomic_load(&queue -> read, &read, __ATOMIC_SEQ_CST);
-    unsigned int cleaned = 0;
     while (node != read) {
+        /* We should never get a nullpointer before reaching read */
+        node = queue -> all;
         queue -> all = node -> next;
         tut_free(node);
-        node = queue -> all;
         cleaned++;
     }
+    pthread_mutex_unlock(&queue -> write_lock);
     return cleaned;
 }
 
